68f451cc8a1035fdb86f3f08e1b17facfe603a16
38752 <118719220+38752@users.noreply.github.com>
2023-01-06 13:51:49 +0900

    hima一覧表示の実装

--- app/controllers/himas_controller.rb
+++ app/controllers/himas_controller.rb
@@ -1,6 +1,7 @@
 class HimasController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
+    @himas = Hima.all.includes(:hima_purposes, :user).order("created_at DESC")
   end
 
   def new
