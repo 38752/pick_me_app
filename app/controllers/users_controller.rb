72196c3b6b59a192854f257c0c935e2456c70160
38752 <118719220+38752@users.noreply.github.com>
2023-01-09 17:11:34 +0900

    usersコントローラとルーティングにedit,updateアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,29 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :user_owner?, only: [:edit, :update]
   def show
-    @user = User.find(params[:id])
+    set_user
+  end
+
+  def edit
+    set_user
+  end
+
+  def update
+    set_user
   end
 
   private
 
-  def message_params
+  def user_owner?
+    redirect_to root_path if params[:id].to_i != current_user.id
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def profile_params
     params.require(:user).permit(:nickname, :how_old_id, :sex_id, :introduction, {images: []}).merge(user_id: current_user.id)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'himas#index'
   resources :himas, only: [:index, :new, :create]
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 end
