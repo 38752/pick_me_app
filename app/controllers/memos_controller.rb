ff17c3f34f693cf48aea2decd8d61f93490279f4
38752 <118719220+38752@users.noreply.github.com>
2023-01-11 10:15:24 +0900

    memosコントローラのアクションの定義

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,4 +1,18 @@
 class MemosController < ApplicationController
   def edit
+    @memo = Memo.find_by(subject_id: params[:id].to_i, object_id: params[:user_id].to_i)
+    if @memo == nil
+      @memo = Memo.create(memo: "", subject_id: params[:id].to_i, object_id: params[:user_id].to_i)
+    end
+  end
+
+  def update
+    @memo = Memo.create(memo_params)
+  end
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:memo).merge(subject_id: params[:id], object_id: params[:user_id])
   end
 end
