50ab224d65921f435cc858210fcc0f48883407ed
38752 <118719220+38752@users.noreply.github.com>
2023-01-12 18:37:29 +0900

    メモ編集フォームの作成とアクションの定義

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,18 +1,30 @@
 class MemosController < ApplicationController
+  before_action :user_owner?
+  before_action :set_memo
   def edit
-    @memo = Memo.find_by(subject_id: params[:id].to_i, object_id: params[:user_id].to_i)
     if @memo == nil
       @memo = Memo.create(memo: "", subject_id: params[:id].to_i, object_id: params[:user_id].to_i)
     end
+    binding.pry
   end
 
   def update
-    @memo = Memo.create(memo_params)
+    if @memo.update(memo_params)
+      redirect_to user_path(@memo.object)
+    end
   end
 
   private
 
+  def user_owner?
+    redirect_to root_path if params[:id].to_i != current_user.id
+  end
+
+  def set_memo
+    @memo = Memo.find_by(subject_id: params[:id].to_i, object_id: params[:user_id].to_i)
+  end
+
   def memo_params
-    params.require(:memo).permit(:memo).merge(subject_id: params[:id], object_id: params[:user_id])
+    params.require(:memo).permit(:memo)
   end
 end
