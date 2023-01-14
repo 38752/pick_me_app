bcba4447f93908056d644a9b249bdeac4ef927d5
38752 <118719220+38752@users.noreply.github.com>
2023-01-14 10:04:42 +0900

    hit_onの保存機能の実装

--- app/controllers/hit_ons_controller.rb
+++ app/controllers/hit_ons_controller.rb
@@ -1,4 +1,20 @@
 class HitOnsController < ApplicationController
   def create
+    hit_on_and_errors = {hit_on: nil, errors: nil}
+    hit_on = HitOn.new(hit_on_params)
+
+    if hit_on.save
+      hit_on_and_errors[:hit_on] = hit_on
+    else
+      hit_on_and_errors[:errors] = hit_on.errors.full_messages
+    end
+    render json:{ hit_on_and_errors: hit_on_and_errors }
+  end
+
+  private
+
+  def hit_on_params
+    hima_owner_id = Hima.find(params[:hima_id]).user.id
+    params.require(:hit_on).permit(:line).merge(hima_id: params[:hima_id], user_id: current_user.id, hima_owner_id: hima_owner_id)
   end
 end
--- app/models/hit_on.rb
+++ app/models/hit_on.rb
@@ -1,9 +1,10 @@
 class HitOn < ApplicationRecord
-  # attr_accessor :hima_owner_id
+  attr_accessor :hima_owner_id
 
   belongs_to :user
   belongs_to :hima
 
-  # validates :user_id, comparison: { other_than: :hima_owner_id }
-  validates :user_id, uniqueness: { scope: :hima_id }
+  validates :line, presence: true
+  validates :user_id, numericality: { other_than: :hima_owner_id, message: "and hima's owner must be different" }
+  validates :user_id, uniqueness: { scope: :hima_id, message: "is allowed to send only 1 request per 1 hima" }
 end
