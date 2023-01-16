4fa55cc42b7a25b794ddd4452e2463bb838597d9
38752 <118719220+38752@users.noreply.github.com>
2023-01-16 17:54:22 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :block_edit_or_delete_account, if: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   private
+
+  def block_edit_or_delete_account
+    if params[:controller] == 'devise/registrations'
+      if params[:action] == 'edit' || params[:action] == 'update' || params[:action] == 'destroy'
+        redirect_to root_path
+      end
+    end
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
