bec25fa12760e90ba660adb71cc020a144bbc9f6
38752 <118719220+38752@users.noreply.github.com>
2023-01-17 17:11:32 +0900

    ビューの作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,16 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :room_participant?
   def index
+    
+  end
+
+  private
+
+  def room_participant?
+    @room = Room.find(params[:room_id])
+    unless @room.users.ids.include?(current_user.id)
+      redirect_to root_path
+    end
   end
 end
