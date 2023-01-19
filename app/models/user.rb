5b5f86700930497d90e31282d8a1ed3e20f4468e
38752 <118719220+38752@users.noreply.github.com>
2023-01-19 14:32:32 +0900

    アソシエーションの修正とビューの細かな修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ApplicationRecord
   belongs_to :sex
   belongs_to :how_old
   belongs_to :status
-  has_many_attached :images
+  has_many_attached :images, dependent: :destroy
 
-  has_many :himas
-  has_many :hit_ons
+  has_many :himas, dependent: :destroy
+  has_many :hit_ons, dependent: :destroy
 
   # Memoモデルとのアソシエーション
   has_many :memos,         class_name: "Memo", foreign_key: "subject_id", dependent: :destroy
@@ -22,11 +22,11 @@ class User < ApplicationRecord
   # subjectがフォローしたり、アンフォローしたりするための記述
   has_many :relationships, class_name: "Relationship", foreign_key: "subject_id", dependent: :destroy
   # フォロー一覧を表示するための記述  sourceは本当はobject_idとなっていてカラム名を示している
-  has_many :objects, through: :relationships, source: :object
+  has_many :objects, through: :relationships, source: :object, dependent: :destroy
 
   has_many :reverse_relationships, class_name: "Relationship", foreign_key: "object_id", dependent: :destroy
   # フォロワー一覧を表示するための記述
-  has_many :subjects, through: :reverse_relationships, source: :subjects
+  has_many :subjects, through: :reverse_relationships, source: :subjects, dependent: :destroy
 
   # room関連のアソシエーション
   has_many :own_rooms, class_name: "Room", foreign_key: "room_master_id", dependent: :destroy
