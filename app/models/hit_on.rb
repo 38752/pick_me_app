dc84c794dbe2ae2e2894732534428a3c606231a4
38752 <118719220+38752@users.noreply.github.com>
2023-01-23 23:29:02 +0900

    hit_onテーブルのバリデーションの追加

--- app/models/hit_on.rb
+++ app/models/hit_on.rb
@@ -1,11 +1,12 @@
 class HitOn < ApplicationRecord
-  attr_accessor :hima_owner_id
+  attr_accessor :hima_owner_id, :this_hima
 
   belongs_to :user
   belongs_to :hima
   has_one :room, dependent: :destroy
 
-  validates :line, presence: true
-  validates :user_id, numericality: { other_than: :hima_owner_id, message: "and hima's owner must be different" }
-  validates :user_id, uniqueness: { scope: :hima_id, message: "is allowed to send only 1 request per 1 hima" }
+  validates :this_hima, numericality: {  equal_to: 10, message: "has already been deleted" }
+  validates :user_id,   numericality: { other_than: :hima_owner_id, message: "and hima's owner must be different" }
+  validates :user_id,   uniqueness: { scope: :hima_id, message: "is allowed to send only 1 request per 1 hima" }
+  validates :line,      presence: true
 end
